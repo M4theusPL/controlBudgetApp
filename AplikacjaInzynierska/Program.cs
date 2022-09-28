e027174700bb4ccbec5314c46f492a7ffa8713ca
mattfruz <mattfruz@gmail.com>
2022-09-28 21:14:31 +0200

    Add login authentication

--- AplikacjaInzynierska/Program.cs
+++ AplikacjaInzynierska/Program.cs
@@ -1,12 +1,20 @@
+using AplikacjaInzynierska.Authentication;
 using AplikacjaInzynierska.Data;
+using AplikacjaInzynierska.Pages;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthenticationCore();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ProtectedSessionStorage>(); 
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddSingleton<UserAccountService>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
