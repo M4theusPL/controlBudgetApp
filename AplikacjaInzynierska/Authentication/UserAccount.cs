e8c8174a7d43310874229aec1835eeddfe2688c8
mattfruz <mattfruz@gmail.com>
2022-10-03 22:55:58 +0200

    Change style in NewTransaction, Login, Index

--- AplikacjaInzynierska/Authentication/UserAccount.cs
+++ AplikacjaInzynierska/Authentication/UserAccount.cs
@@ -4,8 +4,8 @@ namespace AplikacjaInzynierska.Pages
 {
     public class UserAccount
     {
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
--- AplikacjaInzynierska/Authentication/UserSession.cs
+++ AplikacjaInzynierska/Authentication/UserSession.cs
@@ -2,9 +2,8 @@
 {
     public class UserSession
     {
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-
-        public string Role { get; set; } = string.Empty;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
